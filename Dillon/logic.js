5ad58928509268f9db4424b030ece8493486f7af
rb25s13 <rb25s13.dwc@gmail.com>
2021-10-13 19:01:12 -0500

    added code to support dropdown selection by year

--- Dillon/logic.js
+++ Dillon/logic.js
@@ -1,25 +1,75 @@
-let myMap = L.map('map', {
-    center: [37.09, -95.71],
-    zoom: 5,
-});
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
 
 let url = '../fires_clean.csv';
 
-d3.csv('../fires_clean.csv').then(function(data) {
-    console.log(data);
+let selectM = d3.select('.mapContainer');
+
+function optionChanged(sel) {
+    console.log(sel);
+    $('#map').remove();
+    selectM.append('div').attr('id', 'map');
+
+    let myMap = L.map('map', {
+        center: [31.14, -100.27],
+        zoom: 5,
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
     
-    for (let i=0;i<data.length;i++) {
-        let location = [data[i].LATITUDE, data[i].LONGITUDE];
-        if (data[i].FIRE_YEAR == '2015' & data[i].FIRE_SIZE_CLASS == 'A') {
-            L.circle(location, {
-                fillOpacity: 0.75,
-                color: 'rgba(0,0,0,0.1)',
-                fillColor: 'red',
-                radius: 20000
-            }).addTo(myMap);
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = () => {
+        let div = L.DomUtil.create("div", "info legend");
+        let legendInfo = "<p>Class</p>" +
+            "<div class=\"labels\">" +
+            "</div>";
+        div.innerHTML = legendInfo;
+        let categories = ['A','B','C','D','E', 'F', 'G'];
+        let colors = ['#a3f600','#dcf400','#f7db11','#fdb72a','#fca35d','#ff5f65', '#ff020c'];
+        let labels = [];
+        categories.forEach((a, index) => {
+            labels.push("<li><div style=\"background-color: " + colors[index] + "\"></div>&nbsp;&nbsp;" + categories[index] + "</li>");
+        });
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+    legend.addTo(myMap);
+
+    d3.csv(url).then(data => {
+        console.log(data);
+        
+        for (let i=0;i<data.length;i++) {
+            let location = [data[i].LATITUDE, data[i].LONGITUDE];
+            let year = data[i].FIRE_YEAR;
+            let fireSize = data[i].FIRE_SIZE;
+            let fireSizeClass = data[i].FIRE_SIZE_CLASS;
+            let cause = data[i].STAT_CAUSE_DESCR;
+            let color = '';
+            if (fireSizeClass == 'A') {
+              color = '#a3f600';
+            } else if (fireSizeClass == 'B') {
+                color = '#dcf400';
+            } else if (fireSizeClass == 'C') {
+                color = '#f7db11';
+            } else if (fireSizeClass == 'D') {
+                color = '#fdb72a';
+            } else if (fireSizeClass == 'E') {
+                color = '#fca35d';
+            } else if (fireSizeClass == 'F') {
+                color = '#ff5f65';
+            } else {
+              color = '#ff020c';
+            }
+            if (year == sel) {
+                L.circle(location, {
+                    fillOpacity: .5,
+                    color: color,
+                    fillColor: color,
+                    radius: fireSize / 640
+                }).bindPopup(`<h3>${cause}</h3><hr><p>
+                <b>Fire Size Class:</b> ${fireSizeClass}<br/><b>Fire Size:</b> ${fireSize} (Estimate of acres)<br/><b>Location:</b> ${location}</p>`).addTo(myMap);
+            }
         }
-    }
-});
+    });
+
+}
+optionChanged();
